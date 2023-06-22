8f323f66a1740eb9d96f317ac52210e693311535
angadh19 <38089335+angadh19@users.noreply.github.com>
2023-06-22 14:37:54 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_chat import message
-from streamlit_extras.colored_header import colored_header
-from streamlit_extras.add_vertical_space import add_vertical_space
+from streamlit_extras import colored_header
+from streamlit_extras import add_vertical_space
 from hugchat import hugchat
 
 st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
